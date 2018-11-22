76f37d9f3a029acb5f89f87cbd65b46698689545
MarcondesOliveira <marcondescurso@outlook.com>
2018-11-22 02:13:52 -0200

    mines

---
+++ curso-react-native/exercicios/src/App2.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import Simples from "./components/Simples";
+import ParImpar from "./components/ParImpar";
+import { Inverter, MegaSena } from "./components/Multi";
+
+export default class App2 extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Simples texto="Flexivel!!" />
+        <ParImpar numero={16} />
+        <Inverter texto="React Nativo!" />
+        <MegaSena numeros={6} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
---
+++ curso-react-native/exercicios/src/Menu.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { createDrawerNavigator } from "react-navigation";
+
+import Simples from "./components/Simples";
+import ParImpar from "./components/ParImpar";
+import { Inverter, MegaSena } from "./components/Multi";
+import Contador from "./components/Contador";
+import Plataformas from "./components/Plataformas";
+import ValidarProps from "./components/ValidarProps";
+import Evento from "./components/Evento";
+import { Avo } from "./components/ComunicacaoDireta";
+import { TextoSincronizado } from "./components/ComunicacaoIndireta";
+import ListaFlex from "./components/ListaFlex";
+import Flex from "./components/Flex";
+
+export default createDrawerNavigator(
+  {
+    Flex: {
+      screen: Flex
+    },
+    ListaFlex: {
+      screen: ListaFlex,
+      navigationOptions: { title: "Lista (Flex Box)" }
+    },
+    TextoSincronizado: {
+      screen: TextoSincronizado,
+      navigationOptions: { title: "Texto Sincronizado" }
+    },
+    Avo: {
+      screen: () => <Avo nome="João" sobrenome="Silva" />
+    },
+    Evento: {
+      screen: Evento
+    },
+    ValidarProps: {
+      screen: () => <ValidarProps ano={18} />
+    },
+    Plataformas: {
+      screen: Plataformas
+    },
+    Contador: {
+      screen: () => <Contador numeroInicial={1000} />
+    },
+    MegaSena: {
+      screen: () => <MegaSena numeros={8} />,
+      navigationOptions: { title: "Mega Sena" }
+    },
+    Inverter: {
+      screen: () => <Inverter texto="React Nativo!" />
+    },
+    ParImpar: {
+      screen: () => <ParImpar numero={30} />,
+      navigationOptions: { title: "Par & Impar" }
+    },
+    Simples: {
+      screen: () => <Simples texto="Flexivel!!!" />
+    }
+  },
+  { drawerWidth: 300 }
+);
